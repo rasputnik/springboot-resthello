145fb4a300e45a8cd28c7dcc7f1b14636e39eb8c
Dick Davies <dick@hellooperator.net>
2018-02-08 00:09:27 +0000

    really basic hello world rest service

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,21 @@
+package hello;
+
+// simple POJO to store a message and an int
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
